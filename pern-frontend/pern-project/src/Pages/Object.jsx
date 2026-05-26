28246ffe1025c49a189e595229d12cc6ce703455
rahullnct <rahulyadavkv01@gmail.com>
2026-05-26 08:59:27 +0530

    frontend is complete

--- pern-frontend/pern-project/src/Pages/Object.jsx
+++ pern-frontend/pern-project/src/Pages/Object.jsx
@@ -1,6 +1,6 @@
 import { Sparkle, Scissors} from 'lucide-react'
 import React, { useState } from 'react'
-
+import "../CSS_Folder/Object_removal.css"
 function Object() {
   const[ObjectState,setObjectState]=useState('')
   const[remove_Detail,setremove_Detail]=useState({
@@ -20,7 +20,7 @@ function Object() {
     <div className='remove_Object_conatiner'>
      <div className='two_object_containers'>
      <form className='first_object_container' onSubmit={submithandler}>
-       <h2 className='first_object_heading'><Sparkle size={20}/>Obejct Removal</h2>
+       <h2 className='first_object_heading'><Sparkle size={20}/>Object Removal</h2>
        <span className='upload_image_subheading'>Upload Image</span>
        <input
             type='file'
@@ -31,7 +31,7 @@ function Object() {
           />
           <div className='describe_object_removal'>
             <h4 className='object_removol_detail'>Describe Object to remove</h4>
-            <input 
+            <textarea 
             type='text'
             placeholder='e.g. car in background, tree from the image'
             name='remove_object'
@@ -42,15 +42,15 @@ function Object() {
           </div>
           <button className='remove_object_btn'><Scissors size={20}/>Remove Obejct</button>
      </form>
-     <div className='second_article_container'>
-      <h2 className='second_article_heading'><Scissors size={25}/>Remove Objct</h2>
+     <div className='second_object_removal_container'>
+      <h2 className='second_object_removal_heading'><Scissors size={25}/>Remove Objct</h2>
        {
-        objectRemoveImage ? (<div className='article_content'>
+        objectRemoveImage ? (<div className='object_removal_content'>
           Object removal
         </div>) :
-        (<div className='no_article_content'>
+        (<div className='no_object_removal_content'>
         <Scissors size={20}/>
-         <p className='no_article'>Describe which object you want to remove and click on 'Remove Object' to get started</p>
+         <p className='no_object_removal'>Describe which object you want to remove and click on 'Remove Object' to get started</p>
         </div>)
        }
       </div>
--- pern-frontend/pern-project/src/Pages/Resume.jsx
+++ pern-frontend/pern-project/src/Pages/Resume.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Sparkle, FileText } from 'lucide-react'
+import "../CSS_Folder/Resume_review.css"
 function Resume() {
+  const [resumeReview,setresumeReview]=useState(null)
+  const [resumeState, setresumeState]=useState('')
+  const resume_submithandler=(event)=>{
+    event.preventDefault()
+  }
   return (
-    <div>
-      review resume
+   <div className='review_resume_conatiner'>
+     <div className='two_review_resume_containers'>
+     <form className='first_resume_container' onSubmit={resume_submithandler}>
+       <h2 className='first_resume_heading'><Sparkle size={20}/>Resume Review</h2>
+       <span className='upload_image_subheading'>Upload Resume</span>
+       <input
+            type='file'
+            accept='application/pdf'
+            onChange={(event) => setresumeState(event.target.files[0])}
+            className='resume_section'
+            required
+          />
+          <p className='support_resume'>Support PDF,JPG and PNG Formats</p>
+          <button className='review_resume_btn'><FileText size={20}/>Review resume</button>
+     </form>
+     <div className='second_object_removal_container'>
+      <h2 className='second_object_removal_heading'><FileText size={20}/>Analysis Results</h2>
+       {
+        resumeReview ? (<div className='resume_review_content'>
+         Resume_review
+        </div>) :
+        (<div className='no_resume_review_content'>
+        <FileText size={20}/>
+         <p className='no_resume_review'>Upload your resume and click on the 'Resume Review' to get started</p>
+        </div>)
+       }
+      </div>
+     </div>
     </div>
   )
 }
-
 export default Resume
